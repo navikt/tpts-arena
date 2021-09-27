df854aba1ef7b3610ea53bf011ea7b0a5c144f84
Cato Olsen <cato.olsen@nav.no>
2021-09-27 10:44:57 +0200

    Fikset testene (JUnit5).

--- src/test/java/no/nav/tpts/arena/ArenaKafkaControllerTest.java
+++ src/test/java/no/nav/tpts/arena/ArenaKafkaControllerTest.java
@@ -1,7 +1,8 @@
 package no.nav.tpts.arena;
 
-import org.junit.After;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,18 +10,22 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.logging.Level;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-@EmbeddedKafka(controlledShutdown = true)
-public class ArenaKafkaControllerTest {
+@EmbeddedKafka
+class ArenaKafkaControllerTest {
 
     @LocalServerPort
     private int port;
@@ -31,15 +36,15 @@ public class ArenaKafkaControllerTest {
     @Autowired
     private KafkaTemplate<String, String> template;
 
-    @After
-    public void after() {
+    @AfterEach
+    public void afterEach() {
         given()
                 .port(port)
                 .delete("/arena");
     }
 
     @Test
-    public void testSingleReceived() {
+    void testSingleReceived() {
 
         String sent = UUID.randomUUID().toString();
         template.send(config.getConsumer().getTopic(), "testKey", sent);
@@ -55,15 +60,16 @@ public class ArenaKafkaControllerTest {
                         .body()
                         .as(String[].class);
 
-        assertThat(received).hasSize(1);
-        assertThat(received[0]).isEqualTo(sent);
+        assertThat(received)
+                .hasSize(1)
+                .contains(sent);
 
     }
 
     @Test
-    public void testCacheOverflow() {
+    void testQueueOverflow() {
 
-        String[] sent = new String[11];
+        String[] sent = new String[12];
         for (int i = 0; i < sent.length; i++) {
             sent[i] = UUID.randomUUID().toString();
             template.send(config.getConsumer().getTopic(), 1, System.currentTimeMillis(), "testKey" + i, sent[i]);
@@ -80,45 +86,10 @@ public class ArenaKafkaControllerTest {
                         .body()
                         .as(String[].class);
 
-        assertThat(received).hasSizeLessThan(sent.length);
-        for (int i = 0; i < received.length; i++) {
-            assertThat(received[i]).isEqualTo(sent[i]);
-        }
-
-    }
-
-    @Test
-    public void testDeleteAndReceive() {
-
-        String[] sent = new String[20];
-        for (int i = 0; i < sent.length; i++) {
-            sent[i] = UUID.randomUUID().toString();
-            template.send(config.getConsumer().getTopic(), 1, System.currentTimeMillis(), "sentKey" + i, sent[i]);
-        }
-
-        given()
-                .port(port)
-                .delete("/arena")
-                .then()
-                .assertThat()
-                .statusCode(200);
-
-        String[] received =
-                given()
-                        .port(port)
-                        .get("/arena")
-                        .then()
-                        .assertThat()
-                        .statusCode(200)
-                        .extract()
-                        .body()
-                        .as(String[].class);
-
-        assertThat(received).hasSize(10);
-        for (int i = 0; i < received.length; i++) {
-            assertThat(received[i]).isEqualTo(sent[10 + i]);
-        }
-
+        assertThat(received)
+                .hasSizeLessThan(sent.length)
+                .doesNotContain(Arrays.copyOfRange(sent, 0, 2))
+                .containsSequence(Arrays.copyOfRange(sent, 2, sent.length));
 
     }
 
