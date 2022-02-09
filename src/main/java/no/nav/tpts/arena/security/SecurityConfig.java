1a71f887e2babf68651ed44b74a8beaaeeca27f0
sigurdgroneng <sigurdgroneng@nav.no>
2022-02-09 15:49:43 +0100

    Remove cookies

--- src/main/java/no/nav/tpts/arena/security/SecurityConfig.java
+++ src/main/java/no/nav/tpts/arena/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
             .authorizeHttpRequests(authorize -> authorize
                     .anyRequest().authenticated())
             .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
