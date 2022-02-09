2e041df4b732d2027b0f8e60dd103b6085b09bd2
sigurdgroneng <sigurdgroneng@nav.no>
2022-02-09 13:56:26 +0100

    Add missing dependencies, use Oauth2-resource-server config

--- src/main/java/no/nav/tpts/arena/security/SecurityConfig.java
+++ src/main/java/no/nav/tpts/arena/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -11,8 +12,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .anyRequest()
-            .authenticated();
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                    .anyRequest().authenticated())
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
