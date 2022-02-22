f5a30c41961c407252ee51d3d4c64117af0e9734
Cato Olsen <cato.olsen@nav.no>
2022-02-22 15:31:05 +0100

    Added explicit STS client.

--- src/main/java/no/nav/tpts/arena/ArenaSoapService.java
+++ src/main/java/no/nav/tpts/arena/ArenaSoapService.java
@@ -3,6 +3,7 @@ package no.nav.tpts.arena;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.YtelseskontraktV3;
+import no.nav.tpts.sts.StsService;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +12,15 @@ import org.springframework.stereotype.Service;
 public class ArenaSoapService {
 
     private final YtelseskontraktV3 service;
+    private final StsService sts;
 
     boolean ping() {
         try {
+            String token = sts.getAccessToken();
+            if (token != null)
+                token = token.substring(0, 10);
+            log.info("STS test: Got token {}", token);
+
             service.ping();
             return true;
         } catch (Exception e) {
