92373b27238e36e82fd5c218948e61c2f97605ca
sigurdgroneng <sigurdgroneng@nav.no>
2022-02-23 10:11:42 +0100

    Make ping endpoint unprotected

--- src/main/java/no/nav/tpts/arena/ArenaController.java
+++ src/main/java/no/nav/tpts/arena/ArenaController.java
@@ -2,6 +2,7 @@ package no.nav.tpts.arena;
 
 import lombok.RequiredArgsConstructor;
 import no.nav.security.token.support.core.api.Protected;
+import no.nav.security.token.support.core.api.Unprotected;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class ArenaController {
 
     private final ArenaSoapService service;
 
+    @Unprotected
     @GetMapping("soap/ping")
     public void ping() {
         if (!service.ping()) {
