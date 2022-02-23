cc3d3ce60a80bce5ea6d3c94512de543c8434f81
sigurdgroneng <sigurdgroneng@nav.no>
2022-02-23 10:11:15 +0100

    Add missing config

---
+++ src/main/java/no/nav/tpts/arena/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package no.nav.tpts.arena;
+
+import no.nav.security.token.support.core.configuration.MultiIssuerConfiguration;
+import no.nav.security.token.support.core.validation.JwtTokenValidationHandler;
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableJwtTokenValidation
+public class ApplicationConfig {}
