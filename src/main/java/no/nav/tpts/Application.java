0183a1ea8889d48da7c2a55d5060433ab762a02c
Cato Olsen <cato.olsen@nav.no>
2022-03-08 11:06:43 +0100

    Gathering all @Enable* in Application.

--- src/main/java/no/nav/tpts/Application.java
+++ src/main/java/no/nav/tpts/Application.java
@@ -1,11 +1,13 @@
 package no.nav.tpts;
 
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableJwtTokenValidation(ignore = { "org.springdoc", "org.springframework" })
 public class Application {
 
     public static void main(String[] args) {
