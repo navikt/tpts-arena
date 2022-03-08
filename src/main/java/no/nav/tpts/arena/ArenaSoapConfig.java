58cfbf2f85d0a1b996c1329f9a9590f6abef9685
Cato Olsen <cato.olsen@nav.no>
2022-03-08 11:09:55 +0100

    - Refactoring to @Value fields. - Replaced deprecated LoggingOutInterceptor.

--- src/main/java/no/nav/tpts/arena/ArenaSoapConfig.java
+++ src/main/java/no/nav/tpts/arena/ArenaSoapConfig.java
@@ -3,11 +3,8 @@ package no.nav.tpts.arena;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.cxf.CXFClient;
 import no.nav.common.cxf.StsConfig;
-import no.nav.common.utils.EnvironmentUtils;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.YtelseskontraktV3;
-import org.apache.cxf.interceptor.LoggingOutInterceptor;
-import org.apache.cxf.ws.security.wss4j.SamlTokenInterceptor;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.apache.cxf.ext.logging.LoggingOutInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,30 +18,32 @@ public class ArenaSoapConfig {
     @Value("${app.arena.soap.endpoints.ytelseskontrakt}")
     private String ytelseskontraktUrl;
 
+    @Value("${app.arena.soap.sts.url}")
+    private String stsUrl;
+
+    @Value("${app.arena.soap.sts.username}")
+    private String stsUsername;
+
+    @Value("${app.arena.soap.sts.password}")
+    private String stsPassword;
+
     @Bean
-    public YtelseskontraktV3 ytelseskontraktV3(
-            @Value("${app.arena.soap.sts.url}")
-                    String url,
-            @Value("${app.arena.soap.sts.username}")
-                    String username,
-            @Value("${app.arena.soap.sts.password}")
-                    String password
-    ) {
-        StsConfig stsConfig = StsConfig.builder()
-                .url(url)
-                .username(username)
-                .password(password)
-                .build();
+    public YtelseskontraktV3 ytelseskontraktV3() {
+
         log.info("Using URL {} for service {}", ytelseskontraktUrl, YtelseskontraktV3.class.getSimpleName());
         return new CXFClient<>(YtelseskontraktV3.class)
                 .wsdl("classpath:wsdl/tjenestespesifikasjon/no/nav/tjeneste/virksomhet/ytelseskontrakt/v3/Binding.wsdl")
                 .serviceName(new QName("http://nav.no/tjeneste/virksomhet/ytelseskontrakt/v3/Binding", "Ytelseskontrakt_v3"))
                 .endpointName(new QName("http://nav.no/tjeneste/virksomhet/ytelseskontrakt/v3/Binding", "Ytelseskontrakt_v3Port"))
-                //.withHandler(new MDCOutHandler())
                 .withOutInterceptor(new LoggingOutInterceptor())
                 .address(ytelseskontraktUrl)
-                .configureStsForSystemUser(stsConfig)
+                .configureStsForSystemUser(StsConfig.builder()
+                        .url(stsUrl)
+                        .username(stsUsername)
+                        .password(stsPassword)
+                        .build())
                 .build();
+        
     }
 
 }
