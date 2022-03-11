ff0f5138321a0812316fa332b6c834b2963fb114
Cato Olsen <cato.olsen@nav.no>
2022-03-11 15:02:47 +0100

    Minor cleanup.

--- src/main/java/no/nav/tpts/sts/StsService.java
+++ src/main/java/no/nav/tpts/sts/StsService.java
@@ -1,15 +1,17 @@
 package no.nav.tpts.sts;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.RequiredArgsConstructor;
-import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
+@Slf4j
 public class StsService {
 
     private final String url;
@@ -18,12 +20,22 @@ public class StsService {
 
     public String getAccessToken() {
 
-        String uri = UriComponentsBuilder.fromHttpUrl(url).queryParam("grant_type", "client_credentials").queryParam("scope", "openid").toUriString();
+        String uri = UriComponentsBuilder
+            .fromHttpUrl(url)
+            .queryParam("grant_type", "client_credentials")
+            .queryParam("scope", "openid")
+            .toUriString();
         RestTemplate template = new RestTemplate();
         template.setInterceptors(List.of(new BasicAuthenticationInterceptor(username, password)));
-        Map<String, String> response = template.getForObject(uri, Map.class);
-        return response == null ? null : response.get("access_token");
+        Response r = template.getForObject(uri, Response.class);
+        return r == null ? null : r.token;
 
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private static class Response {
+        @JsonProperty("access_token")
+        String token;
     }
 
 }
