8211fb3b0d9729adb110f0128ec30df19a47fbd0
Cato Olsen <cato.olsen@nav.no>
2022-03-17 15:56:54 +0100

    STS testing.

--- src/main/java/no/nav/tpts/arena/ArenaController.java
+++ src/main/java/no/nav/tpts/arena/ArenaController.java
@@ -3,8 +3,8 @@ package no.nav.tpts.arena;
 import lombok.RequiredArgsConstructor;
 import no.nav.security.token.support.core.api.Protected;
 import no.nav.security.token.support.core.api.Unprotected;
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import no.nav.tpts.arena.ytelser.YtelseSak;
+import no.nav.tpts.sts.StsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,6 +18,7 @@ import java.util.List;
 public class ArenaController {
 
     private final ArenaSoapService service;
+    private final StsService sts;
 
     @Unprotected
     @GetMapping("soap/ping")
@@ -31,6 +32,12 @@ public class ArenaController {
     @GetMapping("soap/ytelser/{fnr}")
     public List<YtelseSak> getYtelser(@PathVariable String fnr, @RequestParam(required = false) String fom, @RequestParam(required = false) String tom) {
         return YtelseSak.of(service.getYtelser(fnr, fom, tom));
+    }
+
+    @Unprotected
+    @GetMapping("sts/token")
+    public String getToken() {
+        return sts.getAccessToken();
     }
 
 }
