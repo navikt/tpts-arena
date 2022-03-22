340bd0dc1f3cc3616d3d3497a90edb6150d31ff9
Cato Olsen <cato.olsen@nav.no>
2022-03-22 11:20:18 +0100

    - Fixed common typo in @Value. - Fixed stsConfig being set prior to bean init (e.g. with null values). - Moved WSDL/XSDs to correct folder structure. - Fixed namespace URIs (copied from WSDL).

--- src/main/java/no/nav/tpts/arena/ArenaSoapConfig.java
+++ src/main/java/no/nav/tpts/arena/ArenaSoapConfig.java
@@ -3,9 +3,9 @@ package no.nav.tpts.arena;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.cxf.CXFClient;
 import no.nav.common.cxf.StsConfig;
+import no.nav.tjeneste.virksomhet.tiltakogaktivitet.v1.TiltakOgAktivitetV1;
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.YtelseskontraktV3;
 import org.apache.cxf.ext.logging.LoggingOutInterceptor;
-import no.nav.tjeneste.virksomhet.tiltakogaktivitet.v1.TiltakOgAktivitetV1;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,27 +27,31 @@ public class ArenaSoapConfig {
 
     @Value("${app.arena.soap.sts.password}")
     private String stsPassword;
-    @Value("${app.arena.soap.endpoints.tiltakogaktivitet")
+
+    @Value("${app.arena.soap.endpoints.tiltakogaktivitet}")
     private String tiltakogaktivitetUrl;
 
-    StsConfig stsConfig = StsConfig.builder()
+    private StsConfig getStsConfig() {
+        return StsConfig
+            .builder()
             .url(stsUrl)
             .username(stsUsername)
             .password(stsPassword)
             .build();
+    }
 
     @Bean
     public YtelseskontraktV3 ytelseskontraktV3() {
 
         log.info("Using URL {} for service {}", ytelseskontraktUrl, YtelseskontraktV3.class.getSimpleName());
         return new CXFClient<>(YtelseskontraktV3.class)
-                .wsdl("classpath:wsdl/tjenestespesifikasjon/no/nav/tjeneste/virksomhet/ytelseskontrakt/v3/Binding.wsdl")
-                .serviceName(new QName("http://nav.no/tjeneste/virksomhet/ytelseskontrakt/v3/Binding", "Ytelseskontrakt_v3"))
-                .endpointName(new QName("http://nav.no/tjeneste/virksomhet/ytelseskontrakt/v3/Binding", "Ytelseskontrakt_v3Port"))
-                .withOutInterceptor(new LoggingOutInterceptor())
-                .address(ytelseskontraktUrl)
-                .configureStsForSystemUser(stsConfig)
-                .build();
+            .wsdl("classpath:wsdl/tjenestespesifikasjon/no/nav/tjeneste/virksomhet/ytelseskontrakt/v3/Binding.wsdl")
+            .serviceName(new QName("http://nav.no/tjeneste/virksomhet/ytelseskontrakt/v3/Binding", "Ytelseskontrakt_v3"))
+            .endpointName(new QName("http://nav.no/tjeneste/virksomhet/ytelseskontrakt/v3/Binding", "Ytelseskontrakt_v3Port"))
+            .withOutInterceptor(new LoggingOutInterceptor())
+            .address(ytelseskontraktUrl)
+            .configureStsForSystemUser(getStsConfig())
+            .build();
 
     }
 
@@ -55,12 +59,12 @@ public class ArenaSoapConfig {
     public TiltakOgAktivitetV1 tiltakOgAktivitetV1() {
         log.info("Using URL {} for service {}", tiltakogaktivitetUrl, TiltakOgAktivitetV1.class.getSimpleName());
         return new CXFClient<>(TiltakOgAktivitetV1.class)
-                .wsdl("classpath:wsdl/tjenestespesifikasjon/no/nav/tjeneste/virksomhet/tiltakogaktivitet/v1/Binding.wsdl")
-                .serviceName(new QName("http://nav.no/tjeneste/virksomhet/tiltakogaktivitet/v1/Binding", "<Tiltakogaktivitet_v1>"))
-                .endpointName(new QName("http://nav.no/tjeneste/virksomhet/tiltakogaktivitet/v1/Binding", "Tiltakogaktivitet_v1Port"))
-                .withOutInterceptor(new LoggingOutInterceptor())
-                .address(tiltakogaktivitetUrl)
-                .configureStsForSystemUser(stsConfig)
-                .build();
+            .wsdl("classpath:wsdl/tjenestespesifikasjon/no/nav/tjeneste/virksomhet/tiltakogaktivitet/v1/Binding.wsdl")
+            .serviceName(new QName("http://nav.no/tjeneste/virksomhet/tiltakOgAktivitet/v1/binding", "TiltakOgAktivitet_v1"))
+            .endpointName(new QName("http://nav.no/tjeneste/virksomhet/tiltakOgAktivitet/v1/binding", "TiltakOgAktivitet_v1Port"))
+            .withOutInterceptor(new LoggingOutInterceptor())
+            .address(tiltakogaktivitetUrl)
+            .configureStsForSystemUser(getStsConfig())
+            .build();
     }
 }
