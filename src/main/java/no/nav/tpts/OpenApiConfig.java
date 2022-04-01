7d2359b57541554a048c2564583c655a1df7b999
Cato Olsen <cato.olsen@nav.no>
2022-04-01 09:21:11 +0200

    Replaced static index.html with redirect to Swagger UI.

--- src/main/java/no/nav/tpts/OpenApiConfig.java
+++ src/main/java/no/nav/tpts/OpenApiConfig.java
@@ -6,15 +6,14 @@ import io.swagger.v3.oas.models.security.OAuthFlow;
 import io.swagger.v3.oas.models.security.OAuthFlows;
 import io.swagger.v3.oas.models.security.Scopes;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import no.nav.security.token.support.core.api.Unprotected;
-import org.springdoc.webmvc.ui.SwaggerConfigResource;
-import org.springdoc.webmvc.ui.SwaggerWelcomeCommon;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfig implements WebMvcConfigurer {
 
     @Value("${app.azure.tenantId}")
     private String tenantId;
@@ -25,30 +24,41 @@ public class OpenApiConfig {
     @Value("${app.azure.wellKnown}")
     private String openidConnectUrl;
 
+    /**
+     * Redirect root URL to Swagger UI.
+     *
+     * @see WebMvcConfigurer#addViewControllers(ViewControllerRegistry)
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/swagger-ui/index.html");
+    }
+
     @Bean
     public OpenAPI openAPI() {
+        String authorizationUrl = "https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/authorize";
+        String scope = "api://" + clientId + "/.default";
+        String tokenUrl = "https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/token";
+
         return new OpenAPI()
             .components(
                 new Components()
                     .addSecuritySchemes("oauth",
                         new SecurityScheme()
-                        .type(SecurityScheme.Type.OAUTH2)
-                        .flows(new OAuthFlows()
-                            .authorizationCode(
-                                new OAuthFlow()
-                                    .authorizationUrl("https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/oauth2/v2.0/authorize")
-                                    .scopes(new Scopes().addString(getScope(), "api-access"))
-                                    .tokenUrl("https://login.microsoftonline.com/966ac572-f5b7-4bbe-aa88-c76419c0f851/oauth2/v2.0/token")
+                            .type(SecurityScheme.Type.OAUTH2)
+                            .flows(new OAuthFlows()
+                                .authorizationCode(
+                                    new OAuthFlow()
+                                        .authorizationUrl(authorizationUrl)
+                                        .scopes(new Scopes().addString(scope, "Get access to the API."))
+                                        .tokenUrl(tokenUrl)
+                                )
                             )
-                        )
-                        .openIdConnectUrl(openidConnectUrl)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")
+                            .openIdConnectUrl(openidConnectUrl)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
                     )
             );
     }
 
-    private String getScope() {
-        return "api://" + clientId + "/.default";
-    }
 }
