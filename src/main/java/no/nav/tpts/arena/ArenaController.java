f58c5311541da518dfa636c626bc86e05d15e8a9
Cato Olsen <cato.olsen@nav.no>
2022-04-01 13:03:40 +0200

    Removed unused STS test code.

--- src/main/java/no/nav/tpts/arena/ArenaController.java
+++ src/main/java/no/nav/tpts/arena/ArenaController.java
@@ -5,7 +5,6 @@ import no.nav.security.token.support.core.api.Protected;
 import no.nav.security.token.support.core.api.Unprotected;
 import no.nav.tpts.arena.tiltakogaktiviteter.TiltakOgAktivitet;
 import no.nav.tpts.arena.ytelser.YtelseSak;
-import no.nav.tpts.sts.StsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,7 +18,6 @@ import java.util.List;
 public class ArenaController {
 
     private final ArenaSoapService service;
-    private final StsService sts;
 
     @Unprotected
     @GetMapping("soap/ping")
@@ -35,12 +33,6 @@ public class ArenaController {
         return "protected";
     }
 
-    @Unprotected
-    @GetMapping("test/unprotected")
-    public String dong() {
-        return "unprotected";
-    }
-
     @Protected
     @GetMapping("soap/ytelser/{fnr}")
     public List<YtelseSak> getYtelser(@PathVariable String fnr, @RequestParam(required = false) String fom, @RequestParam(required = false) String tom) {
@@ -51,11 +43,6 @@ public class ArenaController {
     @GetMapping("soap/tiltakOgAktivitet/{fnr}")
     public List<TiltakOgAktivitet> getTiltakOgAktivitet(@PathVariable String fnr) {
         return TiltakOgAktivitet.of(service.getTiltaksaktivitetListe(fnr));
-    }
-    @Unprotected
-    @GetMapping("sts/token")
-    public String getToken() {
-        return sts.getAccessToken();
     }
 
 }
