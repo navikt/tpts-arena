4563dfe2ee7f04e1d0dff525b818b0fe37868076
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-06-08 13:41:24 +0200

    Change accessPolicy for inbound app as this app has been renamed

--- src/main/java/no/nav/tpts/Application.java
+++ src/main/java/no/nav/tpts/Application.java
@@ -1,5 +1,6 @@
 package no.nav.tpts;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +9,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableJwtTokenValidation(ignore = { "org.springdoc", "org.springframework" })
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
+        log.info("Starting app tpts-arena");
         SpringApplication.run(Application.class, args);
     }
 
